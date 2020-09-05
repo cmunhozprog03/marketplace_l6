3bcfcf739e9a421f778a4bd7305cb5ccd712a890
cmunhozprog03 <70407943+cmunhozprog03@users.noreply.github.com>
2020-09-05 12:54:26 -0300

    Aula106

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,15 +1,22 @@
-@extends('layout.front')
+@extends('layouts.front')
 
 @section('content')
 
-
-
-
-@endsection
-@extends('layout.front')
-
-@section('content')
-
-
+<div class="row front">
+    @foreach($products as $key => $product)
+    <div class="col-md-4">
+        <div class="card" style="width: 100%">
+            @if($product->photos->count())
+            <img src="{{asset('storage/' . $product->photos->first()->image)}}" alt="" class="card-img-top">
+            @endif
+            <div class="card-body">
+                <h2 class="card-title">{{$product->name}}</h2>
+                <p class="card-text">{{$product->descripition}}</p>
+            </div>
+        </div>
+    </div>
+    @if(($key + 1) / 3 == 0)</div><div class="row front"></div>@endif
+    @endforeach
+</div>
 
 @endsection
